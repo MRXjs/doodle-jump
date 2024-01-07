450cdeaa92f9a02fdeeecb973400bbb20619a892
MRX <sureshmadawa763@gmail.com>
2024-01-08 00:20:12 +0530

    platform done!

--- public/js/script.js
+++ public/js/script.js
@@ -2,4 +2,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     const grid = document.querySelector(".grid");
     const mrx = document.createElement("div");
+    let isGameOver = false;
+    let mrxLeftSpace = 50;
+    let mrxBottomSpace = 50;
+    function createDoodler() {
+        mrx.classList.add("mrx");
+        grid.appendChild(mrx);
+    }
+    createDoodler();
 });
--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   const grid = document.querySelector(".grid")!;
   const mrx = document.createElement("div");
-});
+  let isGameOver = false;
+  let mrxLeftSpace = 50;
+  let mrxBottomSpace = 50;
 
+  function createDoodler() {
+    mrx.classList.add("mrx");
+    grid.appendChild(mrx);
+  }
+  createDoodler();
+});
